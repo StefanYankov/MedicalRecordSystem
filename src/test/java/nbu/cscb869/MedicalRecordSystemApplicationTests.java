13ac32b4605102197d8366c3dbba9d772cb7e035
Stefan Yankov <syankoff@gmail.com>
2025-04-25 00:12:14 +0300

    Initial create

---
+++ src/test/java/nbu/cscb869/MedicalRecordSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nbu.cscb869;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
