c46c5220770ffedd7c9ff08b46be7f03ea2019c1
Stefan Yankov <syankoff@gmail.com>
2025-05-02 19:55:51 +0300

    reverted back to using @Where for soft delete as per official documentation Integration tests still not working with the @testcontainers Started building the Service layer with the DTOs, and validation

--- src/test/java/nbu/cscb869/data/repositories/unit/PatientRepositoryUnitTests.java
+++ src/test/java/nbu/cscb869/data/repositories/unit/PatientRepositoryUnitTests.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Optional;
 
